b7c36bd5a9715d54b71cebf38862280f31b4c904
petrduba <petr.duba@gmail.com>
2026-05-29 19:20:22 +0200

    Tesena1 zmeny

--- tests/tesena1.spec.ts
+++ tests/tesena1.spec.ts
@@ -13,3 +13,53 @@ test('Prihlášení uživatele', async ({ page }) => {
   await page.getByRole('link', { name: ' Odhlásit' }).click();
 
 });
+
+test('Pridani polozky', async ({ page }) => {
+
+  // Otevreni obchodu
+  await page.goto('http://37.27.17.198:8084/cs/');
+
+  // Klik na produkt
+  await page.getByRole('link', {
+    name: 'Mug The best is yet to come'
+  }).first().click();
+
+  // kontrola URL
+  await expect(page).toHaveURL(/mug-the-best-is-yet-to-come/);
+
+  // pridani do kosiku
+  await page.locator('button[data-button-action="add-to-cart"]').click();
+  
+  //await page.click('button:has-text("Do košíku")');
+  await expect(page.getByText('Produkt byl úspěšně přidán')).toBeVisible({ timeout: 10000 });
+  await page.click('button:has-text("Pokračovat v nákupu")');
+
+  await page.getByText('Clothes').click();
+  await page.getByRole('link', { name: 'Hummingbird printed t-shirt' }).first().click();
+  await expect(page).toHaveURL(/8-barva-bila/);
+  await page.locator('#group_1').selectOption('2'); // M
+  await page.locator('button[data-button-action="add-to-cart"]').click();
+  //await page.click('button:has-text("Do košíku")');
+  await expect(page.getByText('Produkt byl úspěšně přidán')).toBeVisible({ timeout: 10000 });
+  await page.click('button:has-text("Pokračovat v nákupu")');
+  await page.getByRole('link', { name: 'Košík' }).click();
+  await expect(page.getByText('Mug The best is yet to come')).toBeVisible();
+  await expect(page.getByText('Hummingbird printed t-shirt')).toBeVisible();
+  await page.goto('http://37.27.17.198:8084/cs/');
+  
+ 
+});
+
+
+
+// Otevreni obchodu
+  /*await page.goto('http://37.27.17.198:8084/cs/');
+  
+    // Pridani prvni polozky do kosiku
+  await page.getByRole('link', { name: 'Mug The best is yet to come' }).first().click();
+  await expect(page).toHaveURL(/mug-the-best-is-yet-to-come/);
+  async function addToCart(page) {
+  await page.click('button:has-text("Do košíku")');
+  await expect(page.locator('.modal')).toBeVisible();
+});
+*/
